ba077c255b89bcc8ed611820bfadf79843bc1c79
Luck13145 <mildredeitzenlxe46@gmail.com>
2022-09-20 17:50:41 +0800

    Revert "Merge pull request #168 from haha459862/main"
    This reverts commit e8c12d2bf408feb129857e577803d918e613482f, reversing
    changes made to 6b71a63b2554f75ec3fbfaa0a1222073576c9b54.

--- app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
+++ app/src/main/java/com/github/tvbox/osc/ui/activity/LivePlayActivity.java
@@ -173,23 +173,6 @@ public class LivePlayActivity extends BaseActivity {
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        if (mVideoView != null) {
-            mVideoView.resume();
-        }
-    }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (mVideoView != null) {
-            mVideoView.pause();
-        }
-    }
-
-    @Override
     protected void onDestroy() {
         super.onDestroy();
         if (mVideoView != null) {
@@ -401,7 +384,6 @@ public class LivePlayActivity extends BaseActivity {
                     public void onAnimationEnd(Animator animation) {
                         super.onAnimationEnd(animation);
                         tvRightSettingLayout.setVisibility(View.INVISIBLE);
-                        liveSettingGroupAdapter.setSelectedGroupIndex(-1);
                     }
                 });
                 animator.start();
